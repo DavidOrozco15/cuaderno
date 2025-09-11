0c122f630f2ad9b2999b3573845ac2c7977cbccb
DavidOrozco15 <juanodavid94@gmail.com>
2025-09-11 00:29:14 -0500

    EjercicioSoftReview

---
+++ menu.py
@@ -0,0 +1,125 @@
+
+def menu():
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Factorial")
+    print("4. Combinatorias")
+    print("5. Triángulo de Pascal")
+    print("6. Celsius a Fahrenheit")
+    print("7. Fahrenheit a Celsius")
+    print("8. Salir")
+
+def suma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+def resta(num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+def factorial(num):
+    if num < 0:
+        return -1
+    elif num == 0:
+        return 1
+    else:
+        f = 1
+        for i in range(1, num+1):
+            f = f * i
+        
+        return f
+    
+def combinatoria(n, k):
+    resultado = factorial(n) // (factorial(k) * factorial(n - k))
+    return resultado
+
+def trianguloPascal(n):
+    if n < 1:
+        print("No se puede calcular")
+    else:
+        for fila in range(n):
+            for col in range(fila + 1):
+                print(combinatoria(fila, col), end=" ")
+            print("")
+
+def conversorFarenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+def conversorCelsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def validacion(mensaje):
+    valido = False
+    while not valido:
+        valor = input(mensaje)
+        es_numero = True
+
+        for c in valor:
+            if not (c >= '0' and c <= '9'):
+                es_numero = False
+
+        # Validamos que sea número y positivo
+        if es_numero and valor != "" and int(valor) >= 0:
+            valido = True
+        else:
+            print("Entrada inválida. Ingrese un número entero positivo.")
+
+    return int(valor)
+
+isActive = True
+while isActive:
+        menu()
+        opcion = validacion("Seleccione una opción: ")
+        match opcion:
+            case 1:
+                print("Suma")
+                num1 = validacion("Ingrese el primer número: ")
+                num2 = validacion("Ingrese el segundo número: ")
+                print(f"La suma es: {suma(num1, num2)}")
+                input("Presione enter salir al menu")
+            case 2:
+                print("Resta")
+                num1 = validacion("Ingrese el primer número: ")
+                num2 = validacion("Ingrese el segundo número: ")
+                print(f"La resta es: {resta(num1, num2)}")    
+                input("Presione enter salir al menu")
+            case 3:
+                print("Factorial")
+                num = validacion("Ingrese un número: ")
+                if factorial(num) == -1:
+                    print("No se puede calcular el factorial de un número negativo")
+                else:
+                    print(f"El factorial de {num} es: {factorial(num)}")
+                input("Presione enter salir al menu")
+            case 4:
+                print("Combinatoria")
+                n = validacion("Ingrese n: ")
+                k = validacion("Ingrese k: ")
+                if k > n:
+                    print("No se puede calcular")
+                else:
+                    print(f"La combinatoria de {n} y {k} es: {combinatoria(n, k)}")
+                input("Presione enter salir al menu")
+            case 5:
+                print("Triángulo de Pascal")
+                n = validacion("Ingrese el número de filas: ")
+                trianguloPascal(n)
+                input("Presione enter salir al menu")
+            case 6:
+                print("Celsius a Fahrenheit")
+                celsius = validacion("Ingrese la temperatura en Celsius: ")
+                print(f"{celsius}°C son {conversorFarenheit(celsius)}°F")
+                input("Presione enter salir al menu")
+            case 7:
+                print("Fahrenheit a Celsius")
+                fahrenheit = validacion("Ingrese la temperatura en Fahrenheit: ")
+                print(f"{fahrenheit}°F son {conversorCelsius(fahrenheit)}°C")
+                input("Presione enter salir al menu")
+            case 8:
+                print("Saliendo...")
+                isActive = False
+            case _:
+                print("Opción no válida") 
+                input("Presione enter para volver a intentarlo")   
